b1aadbefc8949c16b5c53374a9e025fcd1a05f61
George Gibbs <george@gibbs.pro>
2019-01-15 23:33:47 +0100

    Beta windows support (untested, but should work)

--- bsmodinstaller.py
+++ bsmodinstaller.py
@@ -77,11 +77,11 @@ if __name__ == '__main__':
     for item in mods:
         mod.install(item)
 
-#Checks if running on Windows and skips the IPA.exe installer if so (not yet implemented)
-    if os.name != 'nt':
-        if len(os.listdir('IPA/Backups/Beat Saber')) == 0:
-            prefix = os.path.realpath('../../compatdata/620980/pfx/')
-            os.environ['WINEPREFIX'] = prefix
+    if len(os.listdir('IPA/Backups/Beat Saber')) == 0:
+        if os.name != 'nt':
+            os.environ['WINEPREFIX'] = os.path.realpath('../../compatdata/620980/pfx/')
             subprocess.run('wine ./IPA.exe "Beat Saber.exe"', shell=True)
         else:
-        print("IPA.exe has already been run. Skipping.")
+            subprocess.run('IPA.exe "Beat Saber.exe"', shell=True)
+    else:
+    print("IPA.exe has already been run. Skipping.")
